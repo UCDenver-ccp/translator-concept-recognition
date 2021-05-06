91740e28bd2d4db00423086d92684544d47e8a57
Bill Baumgartner <bill.baumgartner@gmail.com>
2021-05-06 11:48:48 -0600

    add checks to entrypoint so that ai-platform can detect a failure

--- scripts-crf-models/human-phenotype/human-phenotype-crf-entrypoint.sh
+++ scripts-crf-models/human-phenotype/human-phenotype-crf-entrypoint.sh
@@ -5,19 +5,25 @@
 
 # extract the version from the MODEL_VERSIONS file - this is used as part of the file name for the serialized model file
 export VERSION=$(grep "PHENOTYPE.HPO" /home/dev/MODEL_VERSIONS | cut -f 2 -d "=")
+[ $? -eq 0 ] || exit 1
 
 # replace the VERSION placeholder in the properties template file to create the properties file used by train.sh
 envsubst < /home/dev/scripts/properties/hp.properties.template > /home/dev/scripts/properties/hp.properties
+[ $? -eq 0 ] || exit 1
 
 # train the model - model will be written to /home/dev/crf-models/hp-ner-model.ser.gz
 /home/dev/scripts/train.sh
+[ $? -eq 0 ] || exit 1
 
 # evaluate the model on the test set and write results (the standard error output stream) to file
 /home/dev/scripts/test.sh 2> /home/dev/crf-performance/phenotype.out 
+[ $? -eq 0 ] || exit 1
 
 # generate the JSON snippet that contains the evaluation metrics
 /home/dev/scripts/crf-performance-to-json.sh hp LOBO_ET_AL /home/dev/crf-performance/phenotype.out > /home/dev/crf-performance/phenotype_${VERSION}.json
+[ $? -eq 0 ] || exit 1
 
 # gsutil the model and the performance data to cloud storage
 gsutil cp /home/dev/crf-models/hpo-ner-model-${VERSION}.ser.gz gs://translator-text-workflow-dev-public/models/entities/crf/human-phenotype/
+[ $? -eq 0 ] || exit 1
 gsutil cp /home/dev/crf-performance/phenotype_${VERSION}.json gs://translator-text-workflow-dev-public/models/entities/crf/human-phenotype/
--- scripts-crf-models/nlm-chem/nlm-chem-crf-entrypoint.sh
+++ scripts-crf-models/nlm-chem/nlm-chem-crf-entrypoint.sh
@@ -5,23 +5,29 @@
 
 # extract the version from the MODEL_VERSIONS file - this is used as part of the file name for the serialized model file
 export VERSION=$(grep "CHEMICAL.NLMCHEM" /home/dev/MODEL_VERSIONS | cut -f 2 -d "=")
+[ $? -eq 0 ] || exit 1
 
 # replace the VERSION placeholder in the properties template file to create the properties file used by train.sh
 envsubst < /home/dev/scripts/properties/nlm-chem.properties.template > /home/dev/scripts/properties/nlm-chem.properties
+[ $? -eq 0 ] || exit 1
 
 # train the model - model will be written to /home/dev/crf-models/ncbidisease-ner-model.ser.gz
 /home/dev/scripts/train.sh
+[ $? -eq 0 ] || exit 1
 
 # evaluate the model on the test set and write results (the standard error output stream) to file
 /home/dev/scripts/test.sh 2> /home/dev/crf-performance/nlmchem.out 
+[ $? -eq 0 ] || exit 1
 
 # generate the JSON snippet that contains the evaluation metrics
 /home/dev/scripts/crf-performance-to-json.sh chemical NLM_CHEM /home/dev/crf-performance/nlmchem.out > /home/dev/crf-performance/nlmchem_${VERSION}.json
+[ $? -eq 0 ] || exit 1
 
 # TODO: generate JSON for comparison against CRAFT Chebi
 
 
 # gsutil the model and the performance data to cloud storage
 gsutil cp /home/dev/crf-models/nlmchem-ner-model-${VERSION}.ser.gz gs://translator-text-workflow-dev-public/models/entities/crf/nlmchem/
+[ $? -eq 0 ] || exit 1
 gsutil cp /home/dev/crf-performance/nlmchem_${VERSION}.json gs://translator-text-workflow-dev-public/models/entities/crf/nlmchem/
 
